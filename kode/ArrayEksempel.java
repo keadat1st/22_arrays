f2a4403c9a07c515ec90e295f0700ea21d6cc390
JarlTuxen <jart@kea.dk>
2018-03-12 16:04:07 +0100

    noter

--- kode/ArrayEksempel.java
+++ kode/ArrayEksempel.java
@@ -3,22 +3,22 @@ public class ArrayEksempel{
    public static void main(String[] args){
       
       //arrays int[], char[], boolean[], String[] - zero based
-      
-      
+            
       //gennemsnit af ti tal i array
       //refactor med for loop
       
       //array med antal elementer fra input og array.length
       
+      //intitialiser array
       
+      //udskriv elementer i array vha. for loop
+      //refactor med for each
       
       //læg en til alle elementer i array
-      //metode
+      //refactor med metode, der tager array som parameter og returnerer array
       
       //byt om på værdier i to arrays - (t = x, x = y, y = t)
       
-      
-       
    }
 
 
