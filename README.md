2203f5e730972ad288bc0a511a32e74643f5ac72
JarlTuxen <jart@kea.dk>
2018-03-12 15:48:34 +0100

    opgaver

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 #22_arrays
+
+Gennemgang af arrays
+
+## Øvelser
+
+Lav program, der fylder et array med ulige tal
+
+Ændr skolen, så den består af arrays af Student og Teacher - alternativt Car Broker
---
+++ kode/ArrayEksempel.java
@@ -0,0 +1,25 @@
+public class ArrayEksempel{
+
+   public static void main(String[] args){
+      
+      //arrays int[], char[], boolean[], String[] - zero based
+      
+      
+      //gennemsnit af ti tal i array
+      //refactor med for loop
+      
+      //array med antal elementer fra input og array.length
+      
+      
+      
+      //læg en til alle elementer i array
+      //metode
+      
+      //byt om på værdier i to arrays - (t = x, x = y, y = t)
+      
+      
+       
+   }
+
+
+}
