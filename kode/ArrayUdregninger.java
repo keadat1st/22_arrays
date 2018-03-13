c783a5b5caf95ad4ff17ff600bb3fbacbd4de88a
JarlTuxen <jart@kea.dk>
2018-03-13 11:49:16 +0100

    øvelse

---
+++ kode/ArrayUdregninger.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class ArrayUdregninger{
+
+   public static void main(String[] args){
+      
+      Scanner scan = new Scanner(System.in);
+      
+      //læs hvor mange tal
+      int antalTal = 5;
+      antalTal = scan.nextInt();
+      
+      int mitTal;
+      //forløkke
+      //læs næste tal
+         mitTal = scan.nextInt();
+         //gem mitTal i ArrayTal
+      
+      int total; //hold styr på sum til gennemsnit
+      //int stoerst=arrayTal[0]; //hold styr på største til største tal
+      //int mindst=arrayTal[0]; //hold styr på mindste til mindste tal
+      //forløkke, hvor vi regner
+      //for (int i=0;){
+      //hvis arrayTal[i]<mindst så mindst=arrayTal[i]
+      //hvis arrayTal[i]>stoerst så stoerst=arrayTal[i]
+      //total+=arrayTal[i]; hold styr på sum
+      
+      //}
+      
+      //efter løkke
+      //int gennemsnit = total / antalTal;
+      
+   }
+
+}
